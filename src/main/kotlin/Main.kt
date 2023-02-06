602f4228f2e1c89293d36cce0259bccd3fb04aaf
Conor Coker <conorcoker@gmail.com>
2023-02-06 20:30:02 +0000

    Added a menu function to allow the user to choose what to be displayed

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,17 +7,39 @@ val joe = Employee(
 
 fun main(args: Array<String>) {
 
+    var input: Int
 
-    println(joe.getFullNameAndTitle())
-    println("Monthly Salary: ${joe.getMonthlySalary()}")
-    println("Monthly PRSI: ${joe.getMonthlyPRSI()}")
-    println("Monthly PAYE: ${joe.getMonthlyPAYE()}")
-    println("Monthly Gross Pay: ${joe.getGrossMonthlyPay()}")
-    println("Monthly Total Deductions: ${joe.getTotalMonthlyDeductions()}")
-    println("Monthly Net Pay: ${joe.getNetMonthlyPay()}")
-    println(getPayslip())
+    do {
+        input = menu()
+        when (input) {
+            1 -> println("Monthly Salary: ${utils.formatToTwoDecimals(joe.getMonthlySalary())}")
+            2 -> println("Monthly PRSI: ${utils.formatToTwoDecimals(joe.getMonthlyPRSI())}")
+            3 -> println("Monthly PAYE: ${utils.formatToTwoDecimals(joe.getMonthlyPAYE())}")
+            4 -> println("Monthly Gross Pay: ${utils.formatToTwoDecimals(joe.getGrossMonthlyPay())}")
+            5 -> println("Monthly Total Deductions: ${utils.formatToTwoDecimals(joe.getTotalMonthlyDeductions())}")
+            6 -> println("Monthly Net Pay: ${utils.formatToTwoDecimals(joe.getNetMonthlyPay())}")
+            7 -> println(getPayslip())
+            -1 -> println("Exiting App")
+            else -> println("Invalid Option")
+        }
+    } while (input != -1)
+}
 
-
+fun menu(): Int {
+    print(
+        """
+         Employee Menu for ${joe.getFullNameAndTitle()}
+           1. Monthly Salary
+           2. Monthly PRSI
+           3. Monthly PAYE
+           4. Monthly Gross Pay
+           5. Monthly Total Deductions
+           6. Monthly Net Pay
+           7. Full Payslip
+          -1. Exit
+         Enter Option : """
+    )
+    return readln().toInt()
 }
 
 fun getPayslip(): String {
@@ -55,8 +77,6 @@ fun getPayslip(): String {
                 |                NET PAY:${utils.formatToTwoDecimals(joe.getNetMonthlyPay())}                                  |
                 ------------------------------------------------------------------
                 ==>>  """
-
-
 }
 
 
